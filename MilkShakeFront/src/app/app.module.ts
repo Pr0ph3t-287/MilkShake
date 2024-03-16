883d0420a6ecbe138d9743dd45cec56e33488f3a
Stephan du Plessis <s.duplessis9000@gmail.com>
2024-03-17 00:11:08 +0200

    Fixes for Angular update.

--- MilkShakeFront/src/app/app.module.ts
+++ MilkShakeFront/src/app/app.module.ts
@@ -8,7 +8,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
