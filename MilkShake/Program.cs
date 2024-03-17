f330caca1e0861c814860053579aa99592f7e8bf
Stephan du Plessis <s.duplessis9000@gmail.com>
2024-03-18 00:03:43 +0200

    Added CORS, added EnableLegacyTimestampBehaviour for PostGres.

--- MilkShake/Program.cs
+++ MilkShake/Program.cs
@@ -4,9 +4,27 @@ using MilkShake.Data;
 using MilkShake.UnitOfWork;
 using Swashbuckle.AspNetCore;
 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MilkShakeDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("MilkShakeContext") ?? throw new InvalidOperationException("Connection string 'MilkShakeContext' not found.")));
+
+
+builder.Services.AddCors(
+    o => o.AddPolicy("CorsPolicy", builder =>
+    {
+        builder
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials()
+        .WithOrigins(
+            "http://localhost:4200",
+            "https://localhost:7149",
+            "http://127.0.0.1:4200"
+
+    );
+    }));
 
 // Add services to the container.
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -18,12 +36,15 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
