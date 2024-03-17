69b10a69416707b490b96d0551a5caa5cfb0e74b
Pr0ph3t-287 <stephan287@gmail.com>
2024-03-17 13:31:29 +0200

    Revert "Fixes for Angular update."
    This reverts commit 883d0420a6ecbe138d9743dd45cec56e33488f3a.

--- MilkShakeFront/src/app/app.module.ts
+++ MilkShakeFront/src/app/app.module.ts
@@ -8,9 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    NgModule,
-    
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
