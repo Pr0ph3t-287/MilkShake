57bec42e093b1e176b568808103120e958ce3252
Stephan du Plessis <s.duplessis9000@gmail.com>
2024-03-18 22:48:07 +0200

    Fixed model property naming.

--- MilkShakeFront/src/app/models/order-item.model.ts
+++ MilkShakeFront/src/app/models/order-item.model.ts
@@ -1,9 +1,9 @@
 export interface OrderItem {
-  order_item_id?: number;
-  order_id: number;
+  orderItemId?: number;
+  orderId: number;
   description: string;
   quantity: number;
   price: number;
-  created_at?: Date;
-  updated_at?: Date;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
--- MilkShakeFront/src/app/models/order.model.ts
+++ MilkShakeFront/src/app/models/order.model.ts
@@ -1,8 +1,8 @@
 export interface Order {
-  order_id?: number;
-  user_id: number;
-  order_date: Date;
-  total_amount: number;
-  created_at?: Date;
-  updated_at?: Date;
+  orderId?: number;
+  userId: number;
+  orderDate: Date;
+  totalAmount: number;
+  created_At?: Date;
+  updated_At?: Date;
 }
