26baa2516d6246a8aea083c7366d61f291cbba76
Pr0ph3t-287 <stephan287@gmail.com>
2024-03-18 15:29:53 +0200

    Added html for containers, and added shake options and fetching from services on Order component.

--- MilkShakeFront/src/app/pages/order/order.component.ts
+++ MilkShakeFront/src/app/pages/order/order.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Consistency } from 'src/app/models/consistency.model';
+import { Flavor } from 'src/app/models/flavor.model';
+import { Topping } from 'src/app/models/topping.model';
+import { ShakeService } from 'src/app/services/shake.service';
 
 @Component({
   selector: 'app-order',
@@ -10,21 +14,62 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class OrderComponent implements OnInit {
   loginForm: FormGroup;
   currentYear: number = Date.now();
+  consistencies: Array<Consistency> = [];
+  flavors: Array<Flavor> = [];
+  toppings: Array<Topping> = [];
+  shakes: Array<string> = [];
 
   constructor( 
     private formBuilder: FormBuilder,
     private router: Router,
     private activatedRoute: ActivatedRoute,
+    private shakeService: ShakeService,
     ) {       
         // Initialize the FormGroup with form controls and validation
         this.loginForm = this.formBuilder.group({
           email: ['', Validators.required],
-          password: ['', Validators.required]
+          password: ['', Validators.required],
+          consistency: [[]],
+          flavor: [[]],
+          toppings: [[]], // Define toppings control as an array
+          
         });
+
+        for (let i = 0; i < 10; i++) {
+          this.shakes.push("milkshake");
+        }
+        
     }
 
-    ngOnInit(): void {
-    }
+
+  ngOnInit(): void {
+    this.shakeService.getConsistencies().subscribe(
+      (response) => {
+        console.log('Response:', response);
+        this.consistencies = response;
+      },
+      (error) => {
+        console.error('Error:', error);
+      });
+
+      this.shakeService.getFlavors().subscribe(
+        (response) => {
+          console.log('Response:', response);
+          this.flavors = response;
+        },
+        (error) => {
+          console.error('Error:', error);
+        });
+
+        this.shakeService.getToppings().subscribe(
+          (response) => {
+            console.log('Response:', response);
+            this.toppings = response;
+          },
+          (error) => {
+            console.error('Error:', error);
+          });
+  }
 
 
   login(): void {
