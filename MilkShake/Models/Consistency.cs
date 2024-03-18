6ece487d2207e8973167a2d46e03f2912ac2fa7e
Stephan du Plessis <s.duplessis9000@gmail.com>
2024-03-18 18:24:40 +0200

    Added price to consistency model.

--- MilkShake/Models/Consistency.cs
+++ MilkShake/Models/Consistency.cs
@@ -18,6 +18,10 @@ namespace MilkShake.Models
         [Column("description")]
         public string Description { get; set; }
 
+        [Column("price", TypeName = "numeric(10,2)")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal Price { get; set; }
+
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}", ApplyFormatInEditMode = true)]
         [Column("created_at")]
         public DateTime CreatedAt { get; set; }
--- MilkShakeFront/src/app/models/consistency.model.ts
+++ MilkShakeFront/src/app/models/consistency.model.ts
@@ -2,6 +2,7 @@ export interface Consistency {
   consistencyId: number;
   name: string;
   description: string;
+  price: number;
   createdAt: Date;
   updatedAt: Date;
 }
